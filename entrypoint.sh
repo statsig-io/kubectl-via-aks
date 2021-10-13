247dbf122903ff8e3073f80ef77abac96a38097e
joe-statsig <joe@statsig.com>
2021-10-13 16:42:06 -0700

    address code review

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -xe
 echo "az version"
 az version 
 
-az login --service-principal --username $app_id --password $password --tenant $tenant
+az login --service-principal --username $app_id --password $password --tenant $tenant >>/dev/null 2>/dev/null
 
 echo "Writing kubeconfig"
 az aks get-credentials --name aks-web-$tier-$region --resource-group aks-$region >>/dev/null 2>/dev/null
