35dcf90b36c8e3495930daa73661ab4cdc0c1b4d
joe <joe@statsig.com>
2022-01-31 19:24:42 +0000

    update cluster resource naming convention for latest cluster

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,11 @@ az version
 az login --service-principal --username $app_id --password $password --tenant $tenant >>/dev/null 2>/dev/null
 
 echo "Writing kubeconfig"
-az aks get-credentials --name aks-web-$tier-$region --resource-group aks-$region >>/dev/null 2>/dev/null
+if [[ $tier = 'latest' ]]; then
+  az aks get-credentials --name aks-web-$tier-$region --resource-group aks-$tier-$region 
+else
+  az aks get-credentials --name aks-web-$tier-$region --resource-group aks-$region
+fi
 
 echo "Issuing kubectl command"
 kubectl "$@"
